c67fef5516b5dc212e357c9c0d4208077d006c3b
CyberpunkIsNotDead <asuka2ndchild@gmail.com>
2026-02-13 03:56:16 +0300

    Move Button files to dedicated Button directory
    - Create src/common/components/Button/ directory structure
    - Move Button.tsx to Button/index.tsx
    - Move Button.module.scss to Button/Button.module.scss
    - Update Button component to use relative import for styles
    - App.tsx import remains the same (imports from directory)
    - All imports and functionality work correctly

--- src/common/components/Button/index.tsx
+++ src/common/components/Button/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Slot } from '@radix-ui/react-slot';
-import styles from '@/common/components/Button.module.scss';
+import styles from './Button.module.scss';
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   asChild?: boolean;
