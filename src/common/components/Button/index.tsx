ef4d2bb9d4676db3deaf9879c07fee9012c9c3b9
CyberpunkIsNotDead <asuka2ndchild@gmail.com>
2026-02-13 21:08:08 +0300

    refactor button

--- src/common/components/Button/index.tsx
+++ src/common/components/Button/index.tsx
@@ -1,34 +1,21 @@
 import React from 'react';
-import { Slot } from '@radix-ui/react-slot';
 import styles from './Button.module.scss';
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-  asChild?: boolean;
   children: React.ReactNode;
   variant?: 'default' | 'opaque';
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  (
-    {
-      asChild = false,
-      children,
-      variant = 'default',
-      className = '',
-      ...props
-    },
-    ref
-  ) => {
-    const Comp = asChild ? Slot : 'button';
-
+  ({ children, variant = 'default', className = '', ...props }, ref) => {
     return (
-      <Comp
+      <button
         className={`${styles.button} ${styles[`button-${variant}`]} ${className}`}
         ref={ref}
         {...props}
       >
         {children}
-      </Comp>
+      </button>
     );
   }
 );
